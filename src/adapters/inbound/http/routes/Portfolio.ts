6ef51af2afe1e5763cad10a508dae6e3595d335b
Sagameda <randhikatamar@gmail.com>
2025-08-04 14:22:18 +0700

    fix: Defining portfolio route schema

--- src/adapters/inbound/http/routes/Portfolio.ts
+++ src/adapters/inbound/http/routes/Portfolio.ts
@@ -31,63 +31,57 @@ const routes: RouteOptions[] = [
         method: ["GET"],
         url: "/details",
         handler: PortfolioController.GetPortfolioDetailsByIdController,
-        preHandler: [authorizationMiddleware(Permissions.VIEW_OWN_PORTFOLIO)]
-        // schema: {
-        //     tags: ["User"],
-        //     response: Schema.BaseResponse({
-        //         type: "Array of Object",
-        //         message: {
-        //             id: { type: "number" },
-        //             name: { type: "string" },
-        //             portfolio_type: { type: "string" },
-        //             target_allocation: { type: "number" },
-        //             cash_balance: { type: "string" },
-        //             total_value: { type: "string" },
-        //             created_at: { type: "string" },
-        //             updated_at: { type: "string" },
-        //         },
-        //     }),
-        // },
+        preHandler: [authorizationMiddleware(Permissions.VIEW_OWN_PORTFOLIO)],
+        schema: {
+            tags: ["User"],
+            response: Schema.BaseResponse({
+                type: "Object",
+                message: {
+                    id: { type: "number" },
+                    user_id: { type: "number" },
+                    client_id: { type: "number" },
+                    name: { type: "string" },
+                    portfolio_type: { type: "string" },
+                    target_allocation: { type: "number" },
+                    cash_balance: { type: "string" },
+                    total_value: { type: "string" },
+                    created_at: { type: "string" },
+                    updated_at: { type: "string" },
+                },
+            }),
+        },
     },
     {
         method: ["POST"],
         url: "/",
         handler: PortfolioController.CreatePortfolioController,
-        // schema: {
-        //     tags: ["User"],
-        //     body: Schema.BaseRequestSchema("Rakha", {
-        //         first_name: { type: "string", default: "" },
-        //         last_name: { type: "string", default: "" },
-        //         phone: { type: "string", default: "" },
-        //         risk_tolerance: { type: "string", default: "" },
-        //         investment_experience: { type: "string", default: "" },
-        //         annual_income: { type: "string", default: "" },
-        //         net_worth: { type: "string", default: "" },
-        //         investment_goals: { type: "string", default: "" },
-        //         date_of_birth: { type: "string", default: "" },
-        //     }),
-        //     response: Schema.BaseResponse({ type: "Boolean" }),
-        // },
+        schema: {
+            tags: ["User"],
+            body: Schema.BaseRequestSchema("Rakha", {
+                cash_balance: { type: "number", default: "" },
+                name: { type: "string", default: "" },
+                portfolio_type: { type: "string", default: "" },
+                target_allocation: { type: "number", default: "" },
+                total_value: { type: "number", default: "" }
+            }),
+            response: Schema.BaseResponse({ type: "Boolean" }),
+        },
     },
     {
         method: ["PUT"],
         url: "/",
         handler: PortfolioController.UpdatePortfolioController,
-        // schema: {
-        //     tags: ["User"],
-        //     body: Schema.BaseRequestSchema("Rakha", {
-        //         first_name: { type: "string", default: "" },
-        //         last_name: { type: "string", default: "" },
-        //         phone: { type: "string", default: "" },
-        //         risk_tolerance: { type: "string", default: "" },
-        //         investment_experience: { type: "string", default: "" },
-        //         annual_income: { type: "string", default: "" },
-        //         net_worth: { type: "string", default: "" },
-        //         investment_goals: { type: "string", default: "" },
-        //         date_of_birth: { type: "string", default: "" },
-        //     }),
-        //     response: Schema.BaseResponse({ type: "Boolean" }),
-        // },
+        schema: {
+            tags: ["User"],
+            body: Schema.BaseRequestSchema("Rakha", {
+                id: { type: "number", default: "" },
+                name: { type: "string", default: "" },
+                portfolio_type: { type: "string", default: "" },
+                target_allocation: { type: "number", default: "" },
+                total_value: { type: "number", default: "" }
+            }),
+            response: Schema.BaseResponse({ type: "Boolean" }),
+        },
     },
     {
         method: ["PUT"],
